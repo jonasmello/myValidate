c060d1bec9c4a2371439cce33be84e59aa776677
Ramon Barros <ramon@ezoom.com.br>
2013-10-02 15:58:44 -0300

    Fix bug validarCPF e adicionado validarCNPJ.

--- myValidate.js
+++ myValidate.js
@@ -22,6 +22,7 @@ var callbackSubmit = false;
             errorattach: "It is necessary to attach a file.",
             errormail: "Please enter a valid email address.",
             errorcpf: "CPF Inválido",
+            errorcnpj: "CNPJ Inválido",
             erroequal: "Campos %s não são iguais",
             required: "required",
             notification: ".notification",
@@ -60,49 +61,79 @@ var callbackSubmit = false;
 /*########################################################################################################################*/
 /*########################################################################################################################*/
 /*########################################################################################################################*/
-                    function validarCPF(cpf) {
-                        var add, i, rev;
-                            cpf = cpf.replace(/[^\d]+/g,'');
-                            if(cpf === '') { return false; }
-                            // Elimina CPFs invalidos conhecidos
-                            if (cpf.length !== 11 ||
-                                    cpf === "00000000000" ||
-                                    cpf === "11111111111" ||
-                                    cpf === "22222222222" ||
-                                    cpf === "33333333333" ||
-                                    cpf === "44444444444" ||
-                                    cpf === "55555555555" ||
-                                    cpf === "66666666666" ||
-                                    cpf === "77777777777" ||
-                                    cpf === "88888888888" ||
-                                    cpf === "99999999999") {
-                                    return false;
+                    function validarCPF(doc) {
+                        'use strict';
+                        var i, cpf = doc.replace(/\D/g, ''), pattern = /^(\d{1})\1{10}$/, sum, mod, digit;
+
+                        if (cpf.length !== 11) {
+                            return false;
+                        }
+                        if (pattern.test(cpf)) {
+                            return false;
+                        }
+                        sum = 0;
+                        for (i = 0; i < 9; i += 1) {
+                            sum += parseInt(cpf.charAt(i), 10) * (10 - i);
+                        }
+                        mod = sum % 11;
+                        digit = (mod > 1) ? (11 - mod) : 0;
+                        if (parseInt(cpf.charAt(9), 10) !== digit) {
+                            return false;
+                        }
+                        sum = 0;
+                        for (i = 0; i < 10; i += 1) {
+                            sum += parseInt(cpf.charAt(i), 10) * (11 - i);
+                        }
+                        mod = sum % 11;
+                        digit = (mod > 1) ? (11 - mod) : 0;
+                        if (parseInt(cpf.charAt(10), 10) !== digit) {
+                            return false;
+                        }
+                        return true;
+                    }
+                    function validarCNPJ(doc) {
+                        'use strict';
+                        var i, cnpj = doc.replace(/\D/g, ''), pattern = /^(\d{1})\1{13}$/, soma, multiplicador, digitoUm, digitoDois;
+                        if (cnpj.length !== 14) {
+                            return false;
+                        }
+                        if (pattern.test(cnpj)) {
+                            return false;
+                        }
+                        soma = 0;
+                        for (i = 0; i < 12; i += 1) {
+                            /** verifica qual é o multiplicador. Caso o valor do caracter seja entre 0-3, diminui o valor do caracter por 5
+                            * caso for entre 4-11, diminui por 13 **/
+                            multiplicador = (i <= 3 ? 5 : 13) - i;
+
+                            soma += parseInt(cnpj.charAt(i), 10) * multiplicador;
+                        }
+                        soma = soma % 11;
+                        if (soma === 0  || soma === 1) {
+                            digitoUm = 0;
+                        } else {
+                            digitoUm = 11 - soma;
+                        }
+                        if (parseInt(digitoUm, 10) === parseInt(cnpj.charAt(12), 10)) {
+                            soma = 0;
+
+                            for (i = 0; i < 13; i += 1) {
+                                /** verifica qual é o multiplicador. Caso o valor do caracter seja entre 0-4, diminui o valor do caracter por 6
+                                 * caso for entre 4-12, diminui por 14 **/
+                                multiplicador = (i <= 4 ? 6 : 14) - i;
+                                soma += parseInt(cnpj.charAt(i), 10) * multiplicador;
                             }
-                            // Valida 1o digito
-                            add = 0;
-                            for (i=0; i < 9; i = i + 1) {
-                                    add += parseInt(cpf.charAt(i), 10) * (10 - i);
+                            soma = soma % 11;
+                            if (soma === 0 || soma === 1) {
+                                digitoDois = 0;
+                            } else {
+                                digitoDois = 11 - soma;
                             }
-                            rev = 11 - (add % 11);
-                            if (rev === 10 || rev === 11) {
-                                    rev = 0;
+                            if (digitoDois === parseInt(cnpj.charAt(13))) {
+                                return true;
                             }
-                            if (rev !== parseInt(cpf.charAt(9), 10)) {
-                                    return false;
-                            }
-                            // Valida 2o digito
-                            add = 0;
-                            for (i = 0; i < 10; i = i + 1) {
-                                    add += parseInt(cpf.charAt(i), 10) * (11 - i);
-                            }
-                            rev = 11 - (add % 11);
-                            if (rev === 10 || rev === 11) {
-                                    rev = 0;
-                            }
-                            if (rev !== parseInt(cpf.charAt(10), 10)) {
-                                    return false;
-                            }
-                            return true;
+                        }
+                        return false;
                     }
                     var my = this,
                         noValidate = false;
@@ -115,6 +146,7 @@ var callbackSubmit = false;
                                 errormail = $form.data('errormail') ? $form.data('errormail') : my.settings.errormail,
                                 errorattach = $form.data('errorattach') ? $form.data('errorattach') : my.settings.errorattach,
                                 errorcpf = $form.data('errorcpf') ? $form.data('errorcpf') : my.settings.errorcpf,
+                                errorcnpj = $form.data('errorcnpj') ? $form.data('errorcnpj') : my.settings.errorcnpj,
                                 erroequal = $form.data('erroequal') ? $form.data('erroequal') : my.settings.erroequal;
                             my.settings.beforeValidate($form);
                             var $requireds = $(this).find('[data-myrules^="'+my.settings.required+'"]');
@@ -143,6 +175,12 @@ var callbackSubmit = false;
                                     if(!validarCPF($this.val())){
                                         noValidate = true;
                                             $(my.settings.notification).html(errorcpf);
+                                            $this.addClass('error');
+                                    }
+                                }else if($this.filter('[data-myrules*="cnpj"]').length){
+                                    if(!validarCNPJ($this.val())){
+                                        noValidate = true;
+                                            $(my.settings.notification).html(errorcnpj);
                                             $this.addClass('error');
                                     }
                                 }else if($this.is('select')&&$this.val()==(''||0)){
