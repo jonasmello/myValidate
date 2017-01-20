162fa456b1e3709e2b63a9202979e08f08f9e79b
Ramon Barros <contato@ramon-barros.com>
2017-01-20 16:37:00 -0200

    Add validatePassword.

--- src/myValidate.js
+++ src/myValidate.js
@@ -82,6 +82,34 @@
     notification: ".notification", // class para notificação
     errorcolor: "#F00", // cor do erro
     notdisabled: true, // Não retorna campos com disabled
+    bind: 'keyup change',
+    changeBackground: true,
+    backgrounds: [
+        ['#cc0000', '#FFF'],
+        ['#cc3333', '#FFF'],
+        ['#cc6666', '#FFF'],
+        ['#ff9999', '#FFF'],
+        ['#e0941c', '#FFF'],
+        ['#e8a53a', '#FFF'],
+        ['#eab259', '#FFF'],
+        ['#efd09e', '#FFF'],
+        ['#ccffcc', '#FFF'],
+        ['#66cc66', '#FFF'],
+        ['#339933', '#FFF'],
+        ['#006600', '#FFF'],
+        ['#105610', '#FFF']
+    ],
+    passwordValidFrom: 60, // 60%
+    onValidatePassword: function(percentage) {
+      if (this.debug) {
+        console.log(percentage);
+      }
+    },
+    onPasswordStrengthChanged: function(passwordStrength, percentage) {
+      if (this.debug) {
+        console.log(passwordStrength, percentage);
+      }
+    },
     // Função executada antes da validação
     beforeValidate: function() {},
     // Função executada quando ha erro
@@ -96,7 +124,7 @@
       if (this.debug) {
         console.log(event, el, status);
       }
-    }
+    },
   };
 
   /**
@@ -187,149 +215,6 @@
     return false;
   };
 
-  var numbers_array = new Array(),
-      upper_letters_array = new Array(),
-      lower_letters_array = new Array(),
-      special_chars_array = new Array(),
-      pStrengthElementsDefaultStyle = new Array(),
-      settings,
-      methods = {
-        init : function( options, callbacks) {
-
-            settings = $.extend({
-                'bind': 'keyup change',
-                'changeBackground': true,
-                'backgrounds': [
-                    ['#cc0000', '#FFF'], ['#cc3333', '#FFF'], ['#cc6666', '#FFF'], ['#ff9999', '#FFF'],
-                    ['#e0941c', '#FFF'], ['#e8a53a', '#FFF'], ['#eab259', '#FFF'], ['#efd09e', '#FFF'],
-                    ['#ccffcc', '#FFF'], ['#66cc66', '#FFF'], ['#339933', '#FFF'], ['#006600', '#FFF'], ['#105610', '#FFF']
-                ],
-                'passwordValidFrom': 60, // 60%
-                'onValidatePassword': function(percentage) { },
-                'onPasswordStrengthChanged': function(passwordStrength, percentage) { }
-            }, options);
-
-            for(var i = 48; i < 58; i++)
-                numbers_array.push(i);
-            for(i = 65; i < 91; i++)
-                upper_letters_array.push(i);
-            for(i = 97; i < 123; i++)
-                lower_letters_array.push(i);
-            for(i = 32; i < 48; i++)
-                special_chars_array.push(i);
-            for(i = 58; i < 65; i++)
-                special_chars_array.push(i);
-            for(i = 91; i < 97; i++)
-                special_chars_array.push(i);
-            for(i = 123; i < 127; i++)
-                special_chars_array.push(i);
-
-            return this.each($.proxy(function (idx, pStrengthElement) {
-
-                pStrengthElementsDefaultStyle[$(pStrengthElement)] = {
-                    'background': $(pStrengthElement).css('background'),
-                    'color': $(pStrengthElement).css('color')
-                }
-
-                calculatePasswordStrength.call(pStrengthElement);
-
-                $(pStrengthElement).bind(settings.bind, function(){
-                    calculatePasswordStrength.call(pStrengthElement);
-                });
-
-            }, this));
-
-            return this;
-        },
-
-        changeBackground: function(pStrengthElement, passwordStrength) {
-            if (passwordStrength === undefined) {
-                passwordStrength = pStrengthElement;
-                pStrengthElement = $(this);
-            }
-            passwordStrength = passwordStrength > 12 ? 12 : passwordStrength;
-
-            $(pStrengthElement).css({
-                'background-color': settings.backgrounds[passwordStrength][0],
-                'color': settings.backgrounds[passwordStrength][1]
-            });
-        },
-
-        resetStyle: function(pStrengthElement) {
-            $(pStrengthElement).css(pStrengthElementsDefaultStyle[$(pStrengthElement)]);
-        }
-    };
-
-  var ord = function(string) {
-      var str = string + '',
-          code = str.charCodeAt(0);
-      if (0xD800 <= code && code <= 0xDBFF) {
-          var hi = code;
-          if (str.length === 1) {
-            return code;
-          }
-          var low = str.charCodeAt(1);
-          return ((hi - 0xD800) * 0x400) + (low - 0xDC00) + 0x10000;
-      }
-
-      if (0xDC00 <= code && code <= 0xDFFF) {
-          return code;
-      }
-        return code;
-  }
-
-  var calculatePasswordStrength = function(){
-      var passwordStrength    = 0,
-          numbers_found       = 0,
-          upper_letters_found = 0,
-          lower_letters_found = 0,
-          special_chars_found = 0,
-          text = $(this).val().trim();
-
-      passwordStrength += 2 * Math.floor(text.length / 8);
-
-      for(var i = 0; i < text.length; i++) {
-          if($.inArray(ord(text.charAt(i)), numbers_array) != -1 && numbers_found < 2) {
-              passwordStrength++;
-              numbers_found++;
-              continue;
-          }
-          if($.inArray(ord(text.charAt(i)), upper_letters_array) != -1 && upper_letters_found < 2) {
-              passwordStrength++;
-              upper_letters_found++;
-              continue;
-          }
-          if($.inArray(ord(text.charAt(i)), lower_letters_array) != -1 && lower_letters_found < 2) {
-              passwordStrength++;
-              lower_letters_found++;
-              continue;
-          }
-          if($.inArray(ord(text.charAt(i)), special_chars_array) != -1 && special_chars_found < 2) {
-              passwordStrength++;
-              special_chars_found++;
-              continue;
-          }
-      }
-
-      behaviour.call($(this), passwordStrength);
-
-      return passwordStrength;
-   }
-
-  var behaviour = function(passwordStrength) {
-    var strengthPercentage = Math.ceil(passwordStrength * 100 / 12);
-        strengthPercentage = strengthPercentage > 100 ? 100 : strengthPercentage;
-
-    settings.onPasswordStrengthChanged.call($(this), passwordStrength, strengthPercentage);
-    if (strengthPercentage >= settings.passwordValidFrom) {
-        settings.onValidatePassword.call($(this), strengthPercentage);
-    }
-
-    if (settings.changeBackground) {
-        methods.changeBackground.call($(this), passwordStrength);
-    }
-  }
-
   /**
    * Construct funcation
    * @return {void}
@@ -378,7 +263,7 @@
       self.$rules.each(function() {
         var $this = $(this);
         if ($this.not(':disabled') || !$this.attr('disabled')) {
-          if ($this.data('myrules') == '') $this.data('myrules', 'required');
+          if ($this.data('myrules') === '')  { $this.data('myrules', 'required'); }
           $this.removeClass('error');
           $this.parent().removeClass('error');
           //chozen
@@ -395,7 +280,8 @@
               .validateDoc($this)
               .validateSelect($this)
               .validateCheckbox($this)
-              .validateEqual($this);
+              .validateEqual($this)
+              .validatePassword($this);
         }
       });
     }
@@ -608,6 +494,143 @@
         field.addClass('error');
         compare.addClass('error');
       }
+    }
+    return this;
+  };
+
+  /**
+   * Validate password
+   * @param  {object} field
+   * @return {myValidate}
+   */
+  $.myValidate.prototype.validatePassword = function(field) {
+    var self = this,
+        numbers_array = [],
+        upper_letters_array = [],
+        lower_letters_array = [],
+        special_chars_array = [],
+        pStrengthElementsDefaultStyle = [];
+
+    if (field.filter('[data-myrules*="password"]').length) {
+      self.callbackSubmit = false;
+
+      for(var i = 48; i < 58; i++) {
+        numbers_array.push(i);
+      }
+      for(i = 65; i < 91; i++) {
+        upper_letters_array.push(i);
+      }
+      for(i = 97; i < 123; i++) {
+        lower_letters_array.push(i);
+      }
+      for(i = 32; i < 48; i++) {
+        special_chars_array.push(i);
+      }
+      for(i = 58; i < 65; i++) {
+        special_chars_array.push(i);
+      }
+      for(i = 91; i < 97; i++) {
+        special_chars_array.push(i);
+      }
+      for(i = 123; i < 127; i++) {
+        special_chars_array.push(i);
+      }
+
+      var ord = function(string) {
+        var str = string + '',
+            code = str.charCodeAt(0);
+        if (0xD800 <= code && code <= 0xDBFF) {
+          var hi = code;
+          if (str.length === 1) {
+            return code;
+          }
+          var low = str.charCodeAt(1);
+          return ((hi - 0xD800) * 0x400) + (low - 0xDC00) + 0x10000;
+        }
+
+        if (0xDC00 <= code && code <= 0xDFFF) {
+          return code;
+        }
+        return code;
+      };
+
+      var behaviour = function(passwordStrength) {
+        var strengthPercentage = Math.ceil(passwordStrength * 100 / 12);
+            strengthPercentage = strengthPercentage > 100 ? 100 : strengthPercentage;
+
+        self.options.onPasswordStrengthChanged.call($(this), passwordStrength, strengthPercentage);
+        self.notification("Senha fraca");
+        if (strengthPercentage >= self.options.passwordValidFrom) {
+          self.notification("Senha segura");
+          self.callbackSubmit = true;
+          self.options.onValidatePassword.call($(this), strengthPercentage);
+        }
+
+        if (self.options.changeBackground) {
+          if (passwordStrength === undefined) {
+            passwordStrength = $(this);
+          }
+          passwordStrength = passwordStrength > 12 ? 12 : passwordStrength;
+
+          $($(this)).css({
+              'background-color': self.options.backgrounds[passwordStrength][0],
+              'color': self.options.backgrounds[passwordStrength][1]
+          });
+        }
+      };
+
+      var calculatePasswordStrength = function() {
+        var passwordStrength    = 0,
+            numbers_found       = 0,
+            upper_letters_found = 0,
+            lower_letters_found = 0,
+            special_chars_found = 0,
+            text = $(this).val().trim();
+
+        passwordStrength += 2 * Math.floor(text.length / 8);
+
+        for(var i = 0; i < text.length; i++) {
+            if($.inArray(ord(text.charAt(i)), numbers_array) !== -1 && numbers_found < 2) {
+                passwordStrength++;
+                numbers_found++;
+                continue;
+            }
+            if($.inArray(ord(text.charAt(i)), upper_letters_array) !== -1 && upper_letters_found < 2) {
+                passwordStrength++;
+                upper_letters_found++;
+                continue;
+            }
+            if($.inArray(ord(text.charAt(i)), lower_letters_array) !== -1 && lower_letters_found < 2) {
+                passwordStrength++;
+                lower_letters_found++;
+                continue;
+            }
+            if($.inArray(ord(text.charAt(i)), special_chars_array) !== -1 && special_chars_found < 2) {
+                passwordStrength++;
+                special_chars_found++;
+                continue;
+            }
+        }
+
+        behaviour.call($(this), passwordStrength);
+
+        return passwordStrength;
+      };
+
+      return field.each($.proxy(function (idx, pStrengthElement) {
+
+          pStrengthElementsDefaultStyle[$(pStrengthElement)] = {
+              'background': $(pStrengthElement).css('background'),
+              'color': $(pStrengthElement).css('color')
+          };
+
+          calculatePasswordStrength.call(pStrengthElement);
+
+          $(pStrengthElement).bind(self.options.bind, function() {
+              calculatePasswordStrength.call(pStrengthElement);
+          });
+
+      }, field));
     }
     return this;
   };
